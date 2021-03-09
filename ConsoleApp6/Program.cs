e00ffcb909c3ff607c3551db886fc6fdcedda5f5
Jornelle <75078710+Jornelle@users.noreply.github.com>
2021-03-09 22:40:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleProjectForTests
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+            var random = new Random();
+            var matrix = new int[6, 6];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            var result = new int[matrix.GetLength(1)];
+
+            for (int i = 0; i < result.Length; i++)
+                result[i] = 1;
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                    if (matrix[i,j] > 0)
+                    {
+                        result[i] += matrix[j, i];
+                    }
+            Console.WriteLine();
+            foreach (var element in result)
+                Console.Write(element + " ");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
